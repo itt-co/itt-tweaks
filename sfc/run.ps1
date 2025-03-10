5b4a55182d5c472150f87e6b8994b184f620e806
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-03-10 05:24:08 +0200

    Update run.ps1

--- sfc/run.ps1
+++ sfc/run.ps1
@@ -1,15 +1,15 @@
 Write-Host 'This may take a few minutes' -ForegroundColor Yellow
 
-Write-Host 'Running CHKDSK scan...' -ForegroundColor Yellow
+Write-Host 'CHKDSK scan...' -ForegroundColor Yellow
 Chkdsk /scan
 
-Write-Host 'Running System File Checker (SFC)...' -ForegroundColor Yellow
+Write-Host 'System File Checker (SFC)...' -ForegroundColor Yellow
 sfc /scannow
 
-Write-Host 'Running DISM RestoreHealth...' -ForegroundColor Yellow
+Write-Host 'DISM RestoreHealth...' -ForegroundColor Yellow
 DISM /Online /Cleanup-Image /Restorehealth
 
-Write-Host 'Running SFC again to verify repairs...' -ForegroundColor Yellow
+Write-Host 'SFC again to verify repairs...' -ForegroundColor Yellow
 sfc /scannow
 
 Write-Host 'System health check completed' -ForegroundColor Yellow
