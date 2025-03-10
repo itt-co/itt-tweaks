cb61c9a0db57d1be50905cf33c43b9dc745fde43
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-03-10 06:23:46 +0200

    Create run.ps1

---
+++ removexboxapps/run.ps1
@@ -0,0 +1,19 @@
+$AppxPackages = @(
+
+    "Microsoft.XboxApp",
+    "Microsoft.Xbox.TCUI",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.XboxGameOverlay",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.XboxSpeechToTextOverlay"
+)
+
+foreach ($name in $AppxPackages) {
+    Write-Host "[i] Attempting to remove $name"
+    try {
+        Get-AppxPackage -Name $name | Remove-AppxPackage  -ErrorAction SilentlyContinue
+        Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like "*$name*" | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+    } catch {
+        Write-Host "[x] PLEASE USE (WINDOWS POWERSHELL) NOT (TERMINAL POWERSHELL 7) TO UNINSTALL"
+    }
+}
