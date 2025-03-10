71155f28af2aaefd573dd5b4273bb40d7e1f5a85
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-03-10 09:50:34 +0200

    Update run.ps1

--- cleantaskbar/run.ps1
+++ cleantaskbar/run.ps1
@@ -1,9 +1,9 @@
 $Registry = @(
-    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds"; Name = "ShellFeedsTaskbarViewMode"; Type = "DWord"; Value = 2 },
-    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search"; Name = "SearchboxTaskbarMode"; Type = "DWord"; Value = 1 },
-    @{ Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People"; Name = "PeopleBand"; Type = "DWord"; Value = 0 },
-    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"; Name = "HideSCAMeetNow"; Type = "DWord"; Value = 1 },
-    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"; Name = "NoNewsAndInterests"; Type = "DWord"; Value = 1 }
+    @{ Path = "HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Feeds"; Name = "ShellFeedsTaskbarViewMode"; Type = "DWord"; Value = 2 },
+    @{ Path = "HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Search"; Name = "SearchboxTaskbarMode"; Type = "DWord"; Value = 1 },
+    @{ Path = "HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced\\People"; Name = "PeopleBand"; Type = "DWord"; Value = 0 },
+    @{ Path = "HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer"; Name = "HideSCAMeetNow"; Type = "DWord"; Value = 1 },
+    @{ Path = "HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer"; Name = "NoNewsAndInterests"; Type = "DWord"; Value = 1 }
 )
 foreach ($Item in $Registry) {
     Write-Host "[+] Optmize $($Item.Name)"
