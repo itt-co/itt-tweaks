a6906be216a1fe188f10e32e766f7d6934ce5b73
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-03-11 04:32:05 +0200

    Update run.ps1

--- cleantaskbar/run.ps1
+++ cleantaskbar/run.ps1
@@ -1,47 +1,40 @@
 $Registry = @(
-    @{ Path = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\NewsAndInterests\AllowNewsAndInterests"; Name = "value"; Type = "DWord"; Value = 0 },
-    @{ Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds"; Name = "EnableFeeds"; Type = "DWord"; Value = 0 },
-    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name = "ShowTaskViewButton"; Type = "DWord"; Value = 0 },
-    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name = "ShowCortanaButton"; Type = "DWord"; Value = 0 },
-    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"; Name = "NoNewsAndInterests"; Type = "DWord"; Value = 1 },
-    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"; Name = "HideSCAMeetNow"; Type = "DWord"; Value = 1 },
-    @{ Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People"; Name = "PeopleBand"; Type = "DWord"; Value = 1 },
-    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search"; Name = "SearchboxTaskbarMode"; Type = "DWord"; Value = 1 },
+    @{ Path = "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\NewsAndInterests"; Name = "value"; Type = "DWord"; Value = 0 }
+    @{ Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds"; Name = "EnableFeeds"; Type = "DWord"; Value = 0 }
+    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name = "ShowTaskViewButton"; Type = "DWord"; Value = 0 }
+    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"; Name = "ShowCortanaButton"; Type = "DWord"; Value = 0 }
+    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"; Name = "NoNewsAndInterests"; Type = "DWord"; Value = 1 }
+    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"; Name = "HideSCAMeetNow"; Type = "DWord"; Value = 1 }
+    @{ Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People"; Name = "PeopleBand"; Type = "DWord"; Value = 1 }
+    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search"; Name = "SearchboxTaskbarMode"; Type = "DWord"; Value = 1 }
     @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds"; Name = "ShellFeedsTaskbarViewMode"; Type = "DWord"; Value = 0 }
 )
 
 try {
-    # Create HKU drive if it doesn't exist
     if (!(Test-Path 'HKU:\')) {
         New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS | Out-Null
     }
 
-    # Iterate over each registry entry
     foreach ($Reg in $Registry) {
-        if ($Reg.Value -ne "Remove") {
-            if (!(Test-Path $Reg.Path)) {
-                Write-Host -Message "$($Reg.Path) was not found, Creating..."
-                New-Item -Path $Reg.Path -Force | Out-Null
-            }
-            Write-Host -Message "Optimizing $($Reg.Name)..."
+        # Check if the registry key exists, if not create it
+        if (!(Test-Path $Reg.Path)) {
+            Write-Host "$($Reg.Path) was not found, creating..."
+            New-Item -Path $Reg.Path -Force | Out-Null
+        }
+
+        # Check if the property exists, if not create or update it
+        if (!(Get-ItemProperty -Path $Reg.Path -Name $Reg.Name -ErrorAction SilentlyContinue)) {
+            Write-Host "Property $($Reg.Name) not found in $($Reg.Path), creating..."
             New-ItemProperty -Path $Reg.Path -Name $Reg.Name -PropertyType $Reg.Type -Value $Reg.Value -Force | Out-Null
         } else {
-            if ($null -ne $Reg.Name) {
-                # Remove the specific registry value
-                Write-Host -Message "Removing $($Reg.Name) from registry..."
-                Remove-ItemProperty -Path $Reg.Path -Name $Reg.Name -Force -ErrorAction SilentlyContinue
-            } else {
-                # Remove the registry path
-                Write-Host -Message "Removing $($Reg.Path)..."
-                Remove-Item -Path $Reg.Path -Recurse -Force -ErrorAction SilentlyContinue
-            }
+            Write-Host "Property $($Reg.Name) found, updating..."
+            Set-ItemProperty -Path $Reg.Path -Name $Reg.Name -Value $Reg.Value -Force | Out-Null
         }
     }
 } catch {
     Write-Host "An error occurred: $_"
 }
 
-# Restart Explorer
 Write-Host "[+] Restarting Explorer..."
 Stop-Process -Name explorer -Force
 Start-Sleep -Seconds 2
