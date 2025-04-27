170981d5ef9d6c97c6d3c99003ccb7d9228f8ff3
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-04-27 06:04:44 +0300

    Update run.ps1

--- sfc/run.ps1
+++ sfc/run.ps1
@@ -1,10 +1,21 @@
 Write-Host '[i] This may take a few minutes'
-Write-Host '[+] CHKDSK scan...' 
+#Write-Host '[+] CHKDSK scan...' 
+Add-Log -Message '[+] CHKDSK scan...'  -level "info"
 Chkdsk /scan
-Write-Host '[+] System File Checker (SFC)...' 
+#Write-Host '[+] System File Checker (SFC)...' 
+Add-Log -Message '[+] System File Checker (SFC)...'   -level "info"
+
 sfc /scannow
-Write-Host '[+] DISM RestoreHealth...' 
+#Write-Host '[+] DISM RestoreHealth...' 
+
+Add-Log -Message '[+] DISM RestoreHealth...'    -level "info"
+
 DISM /Online /Cleanup-Image /Restorehealth
-Write-Host '[+] SFC again to verify repairs...' 
+#Write-Host '[+] SFC again to verify repairs...' 
+Add-Log -Message '[+] SFC again to verify repairs...'    -level "info"
+
 sfc /scannow
-Write-Host '[√] System health check completed' 
+#Write-Host '[√] System health check completed' 
+
+Add-Log -Message '[√] System health check completed'     -level "info"
+
