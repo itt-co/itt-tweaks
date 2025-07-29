bb77ccc088f05751553c77a62f4068a9d1c42197
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-29 07:00:03 +0300

    Update run.ps1

--- Disable Sticky Keys/run.ps1
+++ Disable Sticky Keys/run.ps1
@@ -1,12 +1,3 @@
 Param($path = "HKCU:\Control Panel\Accessibility\StickyKeys",)
-
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-    [Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Host "This script requires administrator privileges." -ForegroundColor Red
-    Write-Host "Please run WinScript.ps1 as Administrator."
-    Pause
-    Exit
-}
-
 Write-Host "Optimizing  $path" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\StickyKeys" -Name "Flags" -Value "58"
