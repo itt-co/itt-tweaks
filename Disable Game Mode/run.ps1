2d0009102402a08916e92a4803d905663d0e2f5c
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-29 07:15:12 +0300

    Update run.ps1

--- Disable Game Mode/run.ps1
+++ Disable Game Mode/run.ps1
@@ -6,11 +6,8 @@ param(
 try {
     Write-Host "[Info] Optimizing $PathHKLM" -ForegroundColor Cyan
     Set-ItemProperty -Path $PathHKLM -Name "AutoGameModeEnabled" -Value 0 -Type DWord -Force -ErrorAction Stop
-    Write-Host "[Success] Updated $PathHKLM" -ForegroundColor Green
-
     Write-Host "[Info] Optimizing $PathHKCU" -ForegroundColor Cyan
     Set-ItemProperty -Path $PathHKCU -Name "AutoGameModeEnabled" -Value 0 -Type DWord -Force -ErrorAction Stop
-    Write-Host "[Success] Updated $PathHKCU" -ForegroundColor Green
 }
 catch {
     Write-Host "[Error] $($_.Exception.Message)" -ForegroundColor Red
