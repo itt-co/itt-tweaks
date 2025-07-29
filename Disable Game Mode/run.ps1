7b9cb88f5a60dc6d684ba83acb5f5db6867873bb
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-29 08:11:41 +0300

    Update run.ps1

--- Disable Game Mode/run.ps1
+++ Disable Game Mode/run.ps1
@@ -1,14 +1,21 @@
 param(
-    [string]$PathHKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR",
-    [string]$PathHKCU = "HKCU:\SOFTWARE\Microsoft\GameBar"
+    [string[]]$RegistryPaths = @(
+        "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR",
+        "HKCU:\SOFTWARE\Microsoft\GameBar"
+    )
 )
 
 try {
-    Write-Host "[Info] Optimizing $PathHKLM" -ForegroundColor Cyan
-    Set-ItemProperty -Path $PathHKLM -Name "AutoGameModeEnabled" -Value 0 -Type DWord -Force -ErrorAction Stop
-    Write-Host "[Info] Optimizing $PathHKCU" -ForegroundColor Cyan
-    Set-ItemProperty -Path $PathHKCU -Name "AutoGameModeEnabled" -Value 0 -Type DWord -Force -ErrorAction Stop
+    foreach ($path in $RegistryPaths) {
+        if (-not (Test-Path -Path $path)) {
+            Write-Host "[Warning] Path '$path' does not exist. Creating it..." -ForegroundColor Yellow
+            New-Item -Path $path -Force | Out-Null
+        }
+
+        Write-Host "[Info] Optimizing $path" -ForegroundColor Cyan
+        Set-ItemProperty -Path $path -Name "AutoGameModeEnabled" -Value 0 -Type DWord -Force -ErrorAction Stop
+    }
 }
 catch {
-    Write-Host "$($_.Exception.Message)" -ForegroundColor Red
+    Write-Host "[Error] $($_.Exception.Message)" -ForegroundColor Red
 }
