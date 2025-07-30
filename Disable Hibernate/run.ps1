51e6b84f24f49e6bf63280d05523258bcb8fefb7
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-30 13:44:46 +0300

    Create run.ps1

---
+++ Disable Hibernate/run.ps1
@@ -0,0 +1,14 @@
+$registryPath = "HKLM:\System\CurrentControlSet\Control\Session Manager\Power"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Set-ItemProperty -Path $registryPath -Name "HibernateEnabled" -Value 0 -Type DWord
+
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Set-ItemProperty -Path $registryPath -Name "ShowHibernateOption" -Value 0 -Type DWord
+powercfg.exe /hibernate off
