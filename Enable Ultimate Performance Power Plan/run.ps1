713405fdd1849de729d06cf73aaf3032278f6e4a
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-30 12:47:05 +0300

    Update run.ps1

--- Enable Ultimate Performance Power Plan/run.ps1
+++ Enable Ultimate Performance Power Plan/run.ps1
@@ -1,1 +1,2 @@
-powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61; Start-Process powercfg.cpl
+Write-Host "[INFO] Creating a Power plan..."
+powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
