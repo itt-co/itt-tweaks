15695de229c760425f3143d2144124bb6d3b3dc6
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-30 13:40:05 +0300

    Create run.ps1

---
+++ Disable Recall Snapshots in Windows 11 24H/run.ps1
@@ -0,0 +1,14 @@
+$registryPath = "HKCU:\Software\Policies\Microsoft\Windows\WindowsAI"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Set-ItemProperty -Path $registryPath -Name "DisableAIDataAnalysis" -Value 1 -Type DWord
+
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsAI"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Set-ItemProperty -Path $registryPath -Name "DisableAIDataAnalysis" -Value 1 -Type DWord
+Set-ItemProperty -Path $registryPath -Name "AllowRecallEnablement" -Value 0 -Type DWord
