e1d650a7978bf275ad911b7317197679889698cf
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-30 13:22:16 +0300

    Create run.ps1

---
+++ Optimizing NVIDIA GPU settings/run.ps1
@@ -0,0 +1,8 @@
+$registryPath = "HKLM:\Software\NVIDIA Corporation\Global\NvCplApi\Policies"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Set-ItemProperty -Path $registryPath -Name "PowerMizerEnable" -Value 1 -Type DWord
+Set-ItemProperty -Path $registryPath -Name "PowerMizerLevel" -Value 0 -Type DWord
+Set-ItemProperty -Path $registryPath -Name "PowerMizerLevelAC" -Value 0 -Type DWord
