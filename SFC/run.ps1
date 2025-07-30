e0c56dd0c4c50b8e11a29fbbfeada833952d8be5
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-30 12:52:40 +0300

    Update run.ps1

--- SFC/run.ps1
+++ SFC/run.ps1
@@ -1,10 +1,10 @@
-Write-Host '[i] This may take a few minutes' -ForegroundColor Cyan
-Write-Host "[+] CHKDSK scan..." -ForegroundColor Cyan 
+Write-Host '[INFO] This may take a few minutes' -ForegroundColor Cyan
+Write-Host "[INFO] CHKDSK scan..." -ForegroundColor Cyan 
 Chkdsk /scan
-Write-Host "[+] System File Checker (SFC)..." -ForegroundColor Cyan   
+Write-Host "[INFO] System File Checker (SFC)..." -ForegroundColor Cyan   
 sfc /scannow
-Write-Host "[+] DISM RestoreHealth..." -ForegroundColor Cyan   
+Write-Host "[INFO] DISM RestoreHealth..." -ForegroundColor Cyan   
 DISM /Online /Cleanup-Image /Restorehealth
-Write-Host "[+] SFC again to verify repairs..." -ForegroundColor Cyan  
+Write-Host "[INFO] SFC again to verify repairs..." -ForegroundColor Cyan  
 sfc /scannow
-Add-Log -Message "[√] System health check completed" -ForegroundColor Cyan  
+Add-Log -Message "[INFO] System health check completed" -ForegroundColor Cyan
