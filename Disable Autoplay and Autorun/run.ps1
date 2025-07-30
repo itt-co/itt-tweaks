276071df7f2d23f39039ad623c7278e4071a14ab
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-30 13:33:58 +0300

    Create run.ps1

---
+++ Disable Autoplay and Autorun/run.ps1
@@ -0,0 +1,13 @@
+$registryPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Set-ItemProperty -Path $registryPath -Name "DisableAutoplay" -Value 1 -Type DWord
+
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Set-ItemProperty -Path $registryPath -Name "NoDriveTypeAutoRun" -Value 255 -Type DWord
