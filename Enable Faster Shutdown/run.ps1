004648ef38dd66b4aff4e538dd3a6d150f3e8b19
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-30 13:24:13 +0300

    Create run.ps1

---
+++ Enable Faster Shutdown/run.ps1
@@ -0,0 +1,14 @@
+$registryPath = "HKLM:\System\CurrentControlSet\Control"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Set-ItemProperty -Path $registryPath -Name "WaitToKillServiceTimeout" -Value "2000" -Type String
+
+$registryPath = "HKCU:\Control Panel\Desktop"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Set-ItemProperty -Path $registryPath -Name "WaitToKillAppTimeout" -Value "2000" -Type String
+Set-ItemProperty -Path $registryPath -Name "HungAppTimeout" -Value "2000" -Type String
