089a7da134dd21cb2b4afffad564b5e2aaba0e20
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-30 13:53:59 +0300

    Create run.ps1

---
+++ Restore Classic Context Menu Windows 11/run.ps1
@@ -0,0 +1,13 @@
+$registryPath = "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[INFO] Creating $registryPath" -ForegroundColor Cyan
+New-ItemProperty -Path $registryPath -Name "" -Value "" -PropertyType String -Force | Out-Null
+
+$registryPath = "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[INFO] Creating $registryPath" -ForegroundColor Cyan
+New-ItemProperty -Path $registryPath -Name "" -Value "" -PropertyType String -Force | Out-Null
