cceda7b55a880743347630ca22ae759b31b2ffda
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:15:44 +0300

    Update run.ps1

--- Super Control Panel/run.ps1
+++ Super Control Panel/run.ps1
@@ -1,2 +1,2 @@
-Write-Host "[INFO] Creating Super Control Panel Mode folder on Desktop" -ForegroundColor Cyan
+Write-Host "[info] Creating Super Control Panel Mode folder on Desktop" -ForegroundColor Cyan
 New-Item -Path "$env:USERPROFILE\Desktop\GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}" -ItemType Directory
