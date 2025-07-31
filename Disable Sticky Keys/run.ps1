3a827a16e63544d0d45aef3192fea3949fcd21f3
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:11:43 +0300

    Update run.ps1

--- Disable Sticky Keys/run.ps1
+++ Disable Sticky Keys/run.ps1
@@ -2,6 +2,6 @@ Param (
     [string]$path = "HKCU:\Control Panel\Accessibility\StickyKeys"
 )
 
-Write-Host "[INFO] Optimizing  $path" -ForegroundColor Cyan
+Write-Host "[info] Optimizing  $path" -ForegroundColor Cyan
 Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\StickyKeys" -Name "Flags" -Value "58"
-Write-Host "[INFO] Restart required" -ForegroundColor Cyan
+Write-Host "[info] Restart required" -ForegroundColor Cyan
