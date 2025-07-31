8a61d125d16468e78319c4cd8ac4ce2ae18607d5
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:13:14 +0300

    Update run.ps1

--- Enable F8 boot menu options/run.ps1
+++ Enable F8 boot menu options/run.ps1
@@ -1,2 +1,2 @@
-Write-Host "[INFO] Setting boot menu policy to Standard" -ForegroundColor Cyan
+Write-Host "[info] Setting boot menu policy to Standard" -ForegroundColor Cyan
 bcdedit /set bootmenupolicy Standard | Out-Null
