662d9de0e5c780532dcd1919d5fb76723b32c2fd
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:19:29 +0300

    Update run.ps1

--- Remove Copilot Windows 11/run.ps1
+++ Remove Copilot Windows 11/run.ps1
@@ -2,40 +2,40 @@ $registryPath = "HKCU:\Software\Policies\Microsoft\Windows\WindowsCopilot"
 if (-not (Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force | Out-Null
 }
-Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Write-Host "[info] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "TurnOffWindowsCopilot" -Value 1 -Type DWord
 
 $registryPath = "HKCU:\SOFTWARE\Policies\Microsoft\Windows\WindowsCopilot"
 if (-not (Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force | Out-Null
 }
-Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Write-Host "[info] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "TurnOffWindowsCopilot" -Value 1 -Type DWord
 
 $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 if (-not (Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force | Out-Null
 }
-Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Write-Host "[info] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "ShowCopilotButton" -Value 0 -Type DWord
 
 $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Edge"
 if (-not (Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force | Out-Null
 }
-Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Write-Host "[info] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "HubsSidebarEnabled" -Value 0 -Type DWord
 
 $registryPath = "HKCU:\Software\Policies\Microsoft\Windows\Explorer"
 if (-not (Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force | Out-Null
 }
-Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Write-Host "[info] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "DisableSearchBoxSuggestions" -Value 1 -Type DWord
 
 $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer"
 if (-not (Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force | Out-Null
 }
-Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Write-Host "[info] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "DisableSearchBoxSuggestions" -Value 1 -Type DWord
