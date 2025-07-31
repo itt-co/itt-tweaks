3bc2bac5c75c98cd053e6b630fbd26045ef607c2
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 10:18:11 +0300

    Update run.ps1

--- Disable suggestions on Start Menu/run.ps1
+++ Disable suggestions on Start Menu/run.ps1
@@ -4,3 +4,9 @@ if (-not (Test-Path $registryPath)) {
 }
 Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "DisableWindowsConsumerFeatures" -Value 1 -Type DWord
+Write-Host "[INFO] Restarting explorer." -ForegroundColor Cyan
+Stop-Process -processName: Explorer -Force
+Start-Sleep -Seconds 1
+if (-not (Get-Process -processName: Explorer)) {
+    Start-Process explorer.exe
+}
