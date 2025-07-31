1617d9e204fe698def982bdff9e69be976e6ed01
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:15:03 +0300

    Update run.ps1

--- Optimize Network/run.ps1
+++ Optimize Network/run.ps1
@@ -2,6 +2,6 @@ $registryPath = "HKLM:\System\CurrentControlSet\Services\Tcpip\Parameters"
 if (-not (Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force | Out-Null
 }
-Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Write-Host "[info] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "TcpAckFrequency" -Value 1 -Type DWord
 Set-ItemProperty -Path $registryPath -Name "TCPNoDelay" -Value 1 -Type DWord
