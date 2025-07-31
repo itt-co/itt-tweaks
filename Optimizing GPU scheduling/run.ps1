4350178d995a4407f0db8ba0608d32ec92fcada1
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:19:43 +0300

    Update run.ps1

--- Optimizing GPU scheduling/run.ps1
+++ Optimizing GPU scheduling/run.ps1
@@ -2,5 +2,5 @@ $registryPath = "HKLM:\SOFTWARE\CurrentControlSet\Control\GraphicsDrivers"
 if (-not (Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force | Out-Null
 }
-Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Write-Host "[info] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "HwSchMode" -Value 1 -Type DWord
