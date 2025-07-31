eab2b9297abcb6ab6f4ea2fe49812bfffca53f4c
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 03:50:16 +0300

    Update run.ps1

--- Disable Background apps/run.ps1
+++ Disable Background apps/run.ps1
@@ -1,19 +1,18 @@
 $Registry = @(
-    @{ Path = "HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\BackgroundAccessApplications"; Name = "GlobalUserDisabled"; Type = "DWord" },
-    @{ Path = "HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Search"; Name = "BackgroundAppGlobalToggle"; Type = "DWord" }
+    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications"; Name = "GlobalUserDisabled"; Type = "DWord"; Value = 1 },
+    @{ Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search"; Name = "BackgroundAppGlobalToggle"; Type = "DWord"; Value = 0 }
 )
 
 foreach ($Item in $Registry) {
     try {
         if (-not (Test-Path -Path $Item.Path)) {
-            Write-Host "[!] '$($Item.Path)' does not exist. Creating it..." -ForegroundColor Yellow
+            Write-Host "[INFO] '$($Item.Path)' does not exist. Creating it..." -ForegroundColor Yellow
             New-Item -Path $Item.Path -Force | Out-Null
         }
-        
+
         Write-Host "[INFO] Optimizing $($Item.Path)\$($Item.Name)" -ForegroundColor Cyan
-        
-        Set-ItemProperty -Path $Item.Path -Name $Item.Name -Value 0 -Type $Item.Type -Force -ErrorAction Stop
-        
+
+        Set-ItemProperty -Path $Item.Path -Name $Item.Name -Value $Item.Value -Type $Item.Type -Force -ErrorAction Stop
     } catch {
         Write-Host "[x] Failed to apply $($Item.Name): $($_.Exception.Message)" -ForegroundColor Red
     }
