69a450885bafe77ac321ff27d67fc9870975bec3
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:10:58 +0300

    Update run.ps1

--- Disable Homegroup/run.ps1
+++ Disable Homegroup/run.ps1
@@ -1,15 +1,15 @@
 $serviceName = "HomeGroupListener"
 if (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) {
-    Write-Host "[INFO] Stopping $serviceName" -ForegroundColor Yellow
+    Write-Host "[info] Stopping $serviceName" -ForegroundColor Yellow
     Stop-Service -Name $serviceName -Force -ErrorAction SilentlyContinue
-    Write-Host "[INFO] Setting $serviceName startup type to Manual" -ForegroundColor Cyan
+    Write-Host "[info] Setting $serviceName startup type to Manual" -ForegroundColor Cyan
     Set-Service -Name $serviceName -StartupType Manual
 }
 
 $serviceName = "HomeGroupProvider"
 if (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) {
-    Write-Host "[INFO] Stopping $serviceName" -ForegroundColor Yellow
+    Write-Host "[info] Stopping $serviceName" -ForegroundColor Yellow
     Stop-Service -Name $serviceName -Force -ErrorAction SilentlyContinue
-    Write-Host "[INFO] Setting $serviceName startup type to Manual" -ForegroundColor Cyan
+    Write-Host "[info] Setting $serviceName startup type to Manual" -ForegroundColor Cyan
     Set-Service -Name $serviceName -StartupType Manual
 }
