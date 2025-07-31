03d4536eeb32d186d703131774b0a58c592a6f56
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:19:05 +0300

    Update run.ps1

--- Remove Home and Gallery from explorer in Windows 11/run.ps1
+++ Remove Home and Gallery from explorer in Windows 11/run.ps1
@@ -2,17 +2,17 @@ $registryPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanc
 if (-not (Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force | Out-Null
 }
-Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Write-Host "[info] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "1" -Value 1 -Type DWord
 
 $registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}"
 if (Test-Path $registryPath) {
-    Write-Host "[INFO] Removing $registryPath" -ForegroundColor Cyan
+    Write-Host "[info] Removing $registryPath" -ForegroundColor Cyan
     Remove-Item -Path $registryPath -Recurse -Force
 }
 
 $registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}"
 if (Test-Path $registryPath) {
-    Write-Host "[INFO] Removing $registryPath" -ForegroundColor Cyan
+    Write-Host "[info] Removing $registryPath" -ForegroundColor Cyan
     Remove-Item -Path $registryPath -Recurse -Force
 }
