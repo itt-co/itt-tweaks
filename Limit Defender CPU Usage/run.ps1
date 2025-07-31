969ccbb6dd1212dec79bdf75fc2c72e36a883046
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:14:33 +0300

    Update run.ps1

--- Limit Defender CPU Usage/run.ps1
+++ Limit Defender CPU Usage/run.ps1
@@ -2,5 +2,5 @@ $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Scan"
 if (-not (Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force | Out-Null
 }
-Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Write-Host "[info] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "AvgCPULoadFactor" -Value 25 -Type DWord
