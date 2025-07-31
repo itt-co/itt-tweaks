238cf508f31e66f4a4462f09e256106280111684
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:12:15 +0300

    Update run.ps1

--- Disable Windows Transparency/run.ps1
+++ Disable Windows Transparency/run.ps1
@@ -2,5 +2,11 @@ $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personal
 if (-not (Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force | Out-Null
 }
-Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Write-Host "[info] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "EnableTransparency" -Value 0 -Type DWord
+Write-Host "[info] Restarting explorer." -ForegroundColor Cyan
+Stop-Process -processName: Explorer -Force
+Start-Sleep -Seconds 1
+if (-not (Get-Process -processName: Explorer)) {
+    Start-Process explorer.exe
+}
