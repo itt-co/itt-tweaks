41b61aa3f4c6cfaa9080734814c730e1d627a732
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:09:42 +0300

    Update run.ps1

--- Detailed BSoD/run.ps1
+++ Detailed BSoD/run.ps1
@@ -2,6 +2,6 @@ $registryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl"
 if (-not (Test-Path $registryPath)) {
     New-Item -Path $registryPath -Force | Out-Null
 }
-Write-Host "[INFO] Optimizing $registryPath" -ForegroundColor Cyan
+Write-Host "[info] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "DisplayParameters" -Value 1 -Type DWord
 Set-ItemProperty -Path $registryPath -Name "DisableEmoticon" -Value 1 -Type DWord
