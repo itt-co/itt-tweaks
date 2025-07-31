be86d9a1a0b6d83afe68ac2dc50788e4bceb083a
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:17:58 +0300

    Update run.ps1

--- Restore All Windows Services to Default/run.ps1
+++ Restore All Windows Services to Default/run.ps1
@@ -1,3 +1,3 @@
-Write-Host "[INFO] Downloading and executing batch script" -ForegroundColor Cyan
+Write-Host "[info] Downloading and executing batch script" -ForegroundColor Cyan
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/emadadel4/WindowsTweaks/refs/heads/main/test.bat' -OutFile "$env:TEMP\script.bat"
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c %TMP%\script.bat && del /f /q %TMP%\script.bat" -NoNewWindow -Wait
