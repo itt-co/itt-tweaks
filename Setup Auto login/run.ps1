361b390de875d6f9e5e3c81d791ca9dea93eb600
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 11:16:16 +0300

    Update run.ps1

--- Setup Auto login/run.ps1
+++ Setup Auto login/run.ps1
@@ -1,1 +1,3 @@
-curl.exe -ss \"https://live.sysinternals.com/Autologon.exe\" -o $env:temp\\autologin.exe ; cmd /c $env:temp\\autologin.exe /accepteula
+Write-Host "[info] Downloading and running Autologon.exe" -ForegroundColor Cyan
+curl.exe -ss "https://live.sysinternals.com/Autologon.exe" -o $env:temp\autologin.exe
+Start-Process -FilePath "$env:temp\autologin.exe" -ArgumentList "/accepteula" -NoNewWindow -Wait
