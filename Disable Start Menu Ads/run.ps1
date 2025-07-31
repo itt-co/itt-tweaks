1c2781760b754371143be27f87d8920bf9d8470d
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-07-31 08:51:10 +0300

    Update run.ps1

--- Disable Start Menu Ads/run.ps1
+++ Disable Start Menu Ads/run.ps1
@@ -14,8 +14,8 @@ try {
 
         New-ItemProperty -Path $Item.Path -Name $Item.Name -PropertyType $Item.Type -Value $Item.Value -Force | Out-Null
         Write-Host "[INFO] Set $($Item.Name) = $($Item.Value) > $($Item.Path)" -ForegroundColor Cyan
-        Start-Sleep -Seconds 8
     }
+    Start-Sleep -Seconds 5
 }
 catch {
     Write-Host "[!] $($_.Exception.Message)" -ForegroundColor Red
