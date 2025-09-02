78af1b4ed5c55da164bbaa4e99e283c289263c82
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-09-02 04:54:20 +0300

    Update run.ps1

--- Disable Recall Snapshots in Windows 11 24H/run.ps1
+++ Disable Recall Snapshots in Windows 11 24H/run.ps1
@@ -12,3 +12,10 @@ if (-not (Test-Path $registryPath)) {
 Write-Host "[i] Optimizing $registryPath" -ForegroundColor Cyan
 Set-ItemProperty -Path $registryPath -Name "DisableAIDataAnalysis" -Value 1 -Type DWord
 Set-ItemProperty -Path $registryPath -Name "AllowRecallEnablement" -Value 0 -Type DWord
+
+$registryPath = "HKLM:\\SYSTEM\\CurrentControlSet\\Control\\CI\\Policy"
+Write-Host "[i] Optimizing $registryPath" -ForegroundColor Cyan
+Set-ItemProperty -Path $registryPath -Name "VerifiedAndReputablePolicyState" -Value 0 -Type DWord
+
+DISM /Online /Disable-Feature /FeatureName:Recall /Quiet /NoRestart
+Write-Host "[i] Please restart your computer in order for the changes to be fully applied" -ForegroundColor Cyan
