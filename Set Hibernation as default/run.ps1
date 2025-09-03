03f05d13762171795c35c9c4eb8d4e79e86b1add
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-09-04 00:18:18 +0300

    Create run.ps1

---
+++ Set Hibernation as default/run.ps1
@@ -0,0 +1,13 @@
+$registryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\238C9FA8-0AAD-41ED-83F4-97BE242C8F20\7bc4a2f9-d8fc-4469-b07b-33eb785aaca0"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[i] Optimizing $registryPath" -ForegroundColor Cyan
+Set-ItemProperty -Path $registryPath -Name "Attributes" -Value 2 -Type DWord -Force
+
+$registryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\abfc2519-3608-4c2a-94ea-171b0ed546ab\94ac6d29-73ce-41a6-809f-6363ba21b47e"
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+Write-Host "[i] Optimizing $registryPath" -ForegroundColor Cyan
+Set-ItemProperty -Path $registryPath -Name "Attributes" -Value 2 -Type DWord -Force
